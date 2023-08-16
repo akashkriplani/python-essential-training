41af26b99d7320cf5e508517a5a222a96680d86e
Akash Kriplani <akash10@discovery.co.za>
2023-08-16 18:23:16 +0530

    Add code

--- Challenges/messenger.py
+++ Challenges/messenger.py
@@ -7,3 +7,48 @@
 # - Contains a method called "printMessages" that prints all collected messages when it's called.
 
 # You might also consider clearing the message list when "printMessages" is called. 
+
+from datetime import datetime
+
+
+def getCurrentTime():
+    return datetime.now().strftime("%m-%d-%Y %H:%M:%S")
+
+
+class Messenger:
+    def __init__(self, listeners=[]):
+        self.listeners = listeners
+    
+    def send(self, message):
+        for listener in self.listeners:
+            listener.receive(message)
+
+    def receive(self, message):
+        # Must be implemented by extending classes
+        pass
+
+
+class SaveMessages(Messenger):
+    def __init__(self, listeners=[]):
+        super().__init__(listeners)
+        self.messages = []
+        
+    def receive(self, message):
+        self.messages.append({'message': message, 'time': getCurrentTime()})
+        
+    def printMessages(self):
+        for m in self.messages:
+            print(f'Message: "{m["message"]}" Time: {m["time"]}')
+        self.messages = []
+
+listener = SaveMessages()
+
+sender = Messenger([listener])
+
+sender.send('Hello, there! This is the first message')
+
+sender.send('Oh hi! This is the second message!')
+
+sender.send('Hola! This is the third and final message!')
+
+listener.printMessages()
