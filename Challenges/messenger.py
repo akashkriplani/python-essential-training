46fd8d15253de8c5a5ba81927b09fbd3e405a52a
Akash Kriplani <akash10@discovery.co.za>
2023-08-16 18:18:34 +0530

    Add messenger.py

---
+++ Challenges/messenger.py
@@ -0,0 +1,9 @@
+## Extending the Messenger
+
+# Create a class "SaveMessages" that extends the Messenger class that does the following things:
+
+# - Add any messages it receives to a list, along with the time the message was received
+# - Use the provided "getCurrentTime" function so that the received message time is a string
+# - Contains a method called "printMessages" that prints all collected messages when it's called.
+
+# You might also consider clearing the message list when "printMessages" is called. 
